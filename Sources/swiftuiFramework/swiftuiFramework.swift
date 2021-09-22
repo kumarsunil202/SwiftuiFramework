2dfe86dd713e76b48037e4ff0b13dc317099d7d3
Sunil Kumar Samal <kumarsunil202@users.noreply.github.com>
2021-09-22 11:44:52 +0530

    Added Documentation

--- Sources/swiftuiFramework/swiftuiFramework.swift
+++ Sources/swiftuiFramework/swiftuiFramework.swift
@@ -9,6 +9,12 @@ public struct swiftuiFramework {
         @State var placeholder: String
         @Binding var text : String
         
+        
+        //option + Command + /
+        /// This is a textfield that consistent with our design!
+        /// - Parameters:
+        ///   - placeholder: The text you see before you enter any text
+        ///   - text: Binding for the text that gets typed into the field
         public init(placeholder: String, text: Binding<String>) {
             self._placeholder = State(initialValue: placeholder)
             self._text = text
