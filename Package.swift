7d2c46a515e9fb3832c2111a05dac8f2a77358d5
Sunil Kumar Samal <kumarsunil202@users.noreply.github.com>
2021-09-22 10:46:35 +0530

    Added changes to Package.swift

--- Package.swift
+++ Package.swift
@@ -5,10 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "swiftuiFramework",
+    platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "swiftuiFramework",
+            type: .dynamic,
             targets: ["swiftuiFramework"]),
     ],
     dependencies: [
